7a8b5b150a022c423e7eab528b72726467058d56
Ernesto Vega <103270525+ernieve@users.noreply.github.com>
2022-07-18 16:15:33 -0300

    Script triggers y creacion tablas
    Modificacion al script de triggers, agregando triggers afters y otros before, creacion de tablas logs e historico alumno para llevar control

--- Scripts/TriggersVega.sql
+++ Scripts/TriggersVega.sql
@@ -1,10 +1,59 @@
--- Trigger de logs
--- Trigger para el registro de accion eliminar en tabla membresia 
-drop trigger if exists BEF_DEL_membresia_logs;
-CREATE TRIGGER BEF_DEL_membresia_logs
+-- Triggers
+-- Logs MEMBRESIA 
+drop trigger if exists `BEF_DEL_membresia_logs`;
+CREATE TRIGGER `BEF_DEL_membresia_logs`
 BEFORE DELETE ON academia_mambolaye.membresia
 FOR EACH ROW
+INSERT INTO `logs`
+VALUES (NOW(), USER(), DATABASE(), VERSION(), "membresia", "DELETE");
+
+drop trigger if exists `BEF_INS_membresia_logs`;
+CREATE TRIGGER `BEF_INS_membresia_logs`
+BEFORE INSERT ON academia_mambolaye.membresia
+FOR EACH ROW
 INSERT INTO logs
-VALUES (NOW(), USER(), DATABASE(), VERSION(), "membresia", "Delete");
+VALUES (NOW(), USER(), DATABASE(), VERSION(), "membresia", "INSERT");
 
+drop trigger if exists `BEF_UPD_membresia_logs`;
+CREATE TRIGGER `BEF_UPD_membresia_logs`
+BEFORE UPDATE ON academia_mambolaye.membresia
+FOR EACH ROW
+INSERT INTO logs
+VALUES (NOW(), USER(), DATABASE(), VERSION(), "membresia", "UPDATE");
 
+-- Logs ALUMNO
+drop trigger if exists `BEF_DEL_alumno_logs`;
+CREATE TRIGGER `BEF_DEL_alumno_logs`
+BEFORE DELETE ON academia_mambolaye.alumno
+FOR EACH ROW
+INSERT INTO logs
+VALUES (NOW(), USER(), DATABASE(), VERSION(), "alumno", "DELETE");
+
+drop trigger if exists `BEF_INS_alumno_logs`;
+CREATE TRIGGER `BEF_INS_alumno_logs`
+BEFORE INSERT ON academia_mambolaye.alumno
+FOR EACH ROW
+INSERT INTO logs
+VALUES (NOW(), USER(), DATABASE(), VERSION(), "alumno", "INSERT");
+
+drop trigger if exists `BEF_UPD_alumno_logs`;
+CREATE TRIGGER `BEF_UPD_alumno_logs`
+BEFORE UPDATE ON academia_mambolaye.alumno
+FOR EACH ROW
+INSERT INTO logs
+VALUES (NOW(), USER(), DATABASE(), VERSION(), "alumno", "UPDATE");
+
+-- Triggers Historico Alumno
+drop trigger if exists `AFT_INS_alumno_historico`;
+CREATE TRIGGER `AFT_INS_alumno_historico`
+AFTER INSERT ON academia_mambolaye.alumno
+FOR EACH ROW
+INSERT INTO historico_alumno
+VALUES (NEW.dni,NEW.nombre,NEW.apellido,NEW.telefono,NEW.mail,NEW.fecha_nacimiento,NOW(),USER(),'INSERT');
+
+drop trigger if exists `AFT_UPD_alumno_historico`;
+CREATE TRIGGER `AFT_UPD_alumno_historico`
+AFTER UPDATE ON academia_mambolaye.alumno
+FOR EACH ROW
+INSERT INTO historico_alumno
+VALUES (NEW.dni,NEW.nombre,NEW.apellido,NEW.telefono,NEW.mail,NEW.fecha_nacimiento,NOW(),USER(),'UPDATE');
--- Scripts/academia_mambolaye.sql
+++ Scripts/academia_mambolaye.sql
@@ -28,10 +28,11 @@ id_membresia INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 id_alumno INT NOT NULL,
 id_tipo_pago INT NOT NULL,
 fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-FOREIGN KEY (id_alumno) REFERENCES academia_mambolaye.alumno(id_alumno),
-FOREIGN KEY (id_tipo_pago) REFERENCES academia_mambolaye.tipo_pago(id_tipo_pago)
+FOREIGN KEY (id_alumno) REFERENCES academia_mambolaye.alumno(id_alumno) ON DELETE CASCADE,
+FOREIGN KEY (id_tipo_pago) REFERENCES academia_mambolaye.tipo_pago(id_tipo_pago) ON DELETE CASCADE
 );
 
+-- PROFESOR
 CREATE TABLE IF NOT EXISTS profesor(
 id_profesor INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 dni INT NOT NULL,
@@ -41,11 +42,13 @@ mail VARCHAR(120) NOT NULL,
 telefono INT NOT NULL
 );
 
+-- GENERO MUSICAL
 CREATE TABLE IF NOT EXISTS genero_musical(
 id_genero INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nombre_genero VARCHAR(15) NOT NULL
 );
 
+-- ESTILO DE BAILE
 CREATE TABLE IF NOT EXISTS estilo(
 id_estilo INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nombre_estilo VARCHAR(20) NOT NULL,
@@ -53,7 +56,7 @@ id_genero INT NOT NULL,
 FOREIGN KEY (id_genero) REFERENCES academia_mambolaye.genero_musical(id_genero)
 );
 
-
+-- HORARIOS DE CLASES
 CREATE TABLE IF NOT EXISTS horario_clase(
 id_horario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 dia VARCHAR(20) NOT NULL,
@@ -61,6 +64,7 @@ hora_inicio TIME,
 hora_fin TIME
 );
 
+-- DIRECTORES ACADEMIA
 CREATE TABLE IF NOT EXISTS director(
 id_director INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 dni INT NOT NULL,
@@ -68,6 +72,7 @@ nombre VARCHAR(50) NOT NULL,
 apellido VARCHAR(50) NOT NULL 
 );
 
+-- SEDES
 CREATE TABLE IF NOT EXISTS sede(
 id_sede INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nombre_sede VARCHAR(100) NOT NULL,
@@ -77,12 +82,13 @@ id_director INT,
 FOREIGN KEY (id_director) REFERENCES director(id_director)
 );
 
-#Agregando Nivel
+-- NIVEL DE BAILE
 CREATE TABLE IF NOT EXISTS nivel(
 id_nivel INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 descripcion VARCHAR(20) NOT NULL
 );
 
+-- SALONES
 CREATE TABLE IF NOT EXISTS salon(
 id_salon INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nombre_salon VARCHAR(50) NOT NULL,
@@ -90,6 +96,7 @@ id_sede INT NOT NULL,
 FOREIGN KEY (id_sede) REFERENCES academia_mambolaye.sede(id_sede)
 );
 
+-- CLASE
 CREATE TABLE IF NOT EXISTS clase(
 id_clase INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 id_profesor INT NOT NULL,
@@ -104,6 +111,7 @@ FOREIGN KEY (id_salon) REFERENCES academia_mambolaye.salon(id_salon),
 FOREIGN KEY (id_nivel) REFERENCES academia_mambolaye.nivel(id_nivel)
 );
 
+-- INSCRIPCION
 CREATE TABLE IF NOT EXISTS inscripcion(
 id_membresia INT NOT NULL,
 id_clase INT NOT NULL,
@@ -120,4 +128,17 @@ CREATE TABLE IF NOT EXISTS logs(
     version VARCHAR(20),
     tabla VARCHAR(20),
     accion VARCHAR(15)
+);
+
+#BITACORA_ALUMNOS
+CREATE TABLE IF NOT EXISTS historico_alumno(
+dni INT NOT NULL,
+nombre VARCHAR(50) NOT NULL,
+apellido VARCHAR(50) NOT NULL,
+telefono INT NOT NULL,
+mail VARCHAR(120) NOT NULL,
+fecha_nacimiento DATE NOT NULL,
+fecha_hora_accion DATETIME,
+usuario VARCHAR(100),
+accion VARCHAR(15)
 );
